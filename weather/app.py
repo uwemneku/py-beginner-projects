edf4374a254a983d6567ba03810661062d294417
Uwem Israel <iamuwem@gmail.com>
2022-09-21 11:21:23 +0100

    used python dotenv package to hide api key for weather app

--- weather/app.py
+++ weather/app.py
@@ -1,12 +1,21 @@
 import requests
+from dotenv import load_dotenv, dotenv_values
 
-API_KEY = "d51d90fab628a5916b6d0867d023cadc"
+# Load environmental variables
+load_dotenv()
+env_variables = dotenv_values(".env")
+
+API_KEY = env_variables["API_KEY"]
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+# Get user input
 lat = input("Enter a latitude: ")
 lon = input("Enter a longitude: ")
+
+# Make API requests
 request_url = f"{BASE_URL}?appid={API_KEY}&lat={lat}&lon={lon}"
 response = requests.get(request_url)
+
 
 if response.status_code == 200:
     data = response.json()
